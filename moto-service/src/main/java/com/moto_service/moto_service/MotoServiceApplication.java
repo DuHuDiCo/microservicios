ac9811e15260c3489a66a48ec182d15efeac012b
Duvan Diaz <dudicodiaz@gmail.com>
2022-11-03 17:23:59 -0500

    commit 03/11/2022

--- moto-service/src/main/java/com/moto_service/moto_service/MotoServiceApplication.java
+++ moto-service/src/main/java/com/moto_service/moto_service/MotoServiceApplication.java
@@ -2,8 +2,10 @@ package com.moto_service.moto_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MotoServiceApplication {
 
 	public static void main(String[] args) {
